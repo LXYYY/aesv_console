8becc491b9398711db2099f26efc9bfc5fe58603
Mike Xiangyu Liu <xiangyu002@e.ntu.edu.sg>
2022-06-01 18:22:36 +0800

    change to maven, init commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module ntu.aesv_console {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires javafx.graphics;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.dataformat.yaml;
+
+    opens ntu.aesv_console to javafx.fxml;
+    exports ntu.aesv_console;
+}
