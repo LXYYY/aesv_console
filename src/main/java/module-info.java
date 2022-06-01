6ebe0fb08caa191e07f924481d60790c16860a8f
Mike Xiangyu Liu <xiangyu002@e.ntu.edu.sg>
2022-06-01 19:33:19 +0800

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,7 +6,6 @@ module ntu.aesv_console {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    requires validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
